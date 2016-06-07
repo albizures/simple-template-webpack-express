b0d7bf407dc29d7e6611b2f30eac08c38dd3d3eb
Jose Albizures <albizures3601@gmail.com>
2016-06-06 19:23:50 -0600

    init template for react

--- src/client/index.js
+++ src/client/index.js
@@ -1,3 +1,13 @@
 
 require('./style/index.styl');
+const React = require('react');
+const ReactDOM = require('react-dom');
 console.log('it\'s working');
+
+const Hello = React.createClass({
+  render() {
+    return <h1> hello </h1>;
+  }
+});
+
+ReactDOM.render(<Hello/>, document.getElementById('root'));
